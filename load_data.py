9e842baa4366d3c6d726baf708d52835321b53fa
Sedrick Keh <sedrickkeh@gmail.com>
2022-05-19 01:21:05 -0400

    add inference script

--- load_data.py
+++ load_data.py
@@ -8,15 +8,16 @@ def load_bible_data(data_dir, train_languages, val_languages):
     for lang in val_languages:
         valid_files.append(f'{data_dir}/{lang}/valid.csv')
 
-    ds_train = load_dataset('csv', data_files=train_files)
-    ds_valid = load_dataset('csv', data_files=valid_files)
-
     langs_dict = {}
+    if len(train_files)>0:      # 0 during inference
+        ds_train = load_dataset('csv', data_files=train_files)
+        langs_dict["train"] = ds_train['train']
+    ds_valid = load_dataset('csv', data_files=valid_files)
+    langs_dict["valid"] = ds_valid['train']
+
     for lang in val_languages:
         test_d = load_dataset('csv', data_files=[f'{data_dir}/{lang}/test.csv'])
         langs_dict[lang] = test_d['train']
-    langs_dict["train"] = ds_train['train']
-    langs_dict["valid"] = ds_valid['train']
 
     raw_datasets = DatasetDict(langs_dict)
     return raw_datasets
---
+++ transformer-inference.py
@@ -0,0 +1,109 @@
+import argparse 
+from transformers import GPT2Tokenizer, AutoConfig, GPT2LMHeadModel
+from transformers import DataCollatorForLanguageModeling, TrainingArguments, Trainer
+from load_data import load_bible_data
+import logging
+logging.basicConfig(level=logging.INFO)
+
+blue = ["acu","alb","ceb","cjp","dik","eng","ewe","gla","hun","jak","kek","mam","nor","por","quw","slk","spa","tgl","vie","zul"]
+green = ["ake","cak","chq","deu","dop","eus","gbi","hrv","ita","kbh","lit","nld","pol","quc","shi","som","swe","usp","wol","xho"]
+red = ["afr","agr","amu","ces","cni","dje","epo","fin","glv","ind","jiv","lat","mri","pck","pot","rom","slv","srp","tmh","wal"]
+yellow = ["bsn","cha","dan","djk","est","fra","hat","isl","kab","lav","nhg","plt","ppk","ron","sna","ssw","tur"]
+langs = blue+green+red+yellow
+langs_dict = {'blue':blue, 'green':green, 'red':red, 'yellow':yellow}
+
+def main(args):
+    raw_datasets = load_bible_data(args.data_dir, args.train_languages, args.test_languages)
+
+    tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+    tokenizer.pad_token = tokenizer.eos_token
+
+    def tokenize(element):
+        outputs = tokenizer(
+            element["text"],
+            truncation=True,
+            max_length=64,
+            pad_to_max_length=True,
+            return_overflowing_tokens=True,
+            return_length=True,
+        )
+        input_batch = []
+        for length, input_ids in zip(outputs["length"], outputs["input_ids"]):
+            input_batch.append(input_ids)
+        return {"input_ids": input_batch}
+
+    tokenized_datasets = raw_datasets.map(
+        tokenize, batched=True, remove_columns=raw_datasets["valid"].column_names
+    )
+
+    config = AutoConfig.from_pretrained(
+            "gpt2",
+            vocab_size=len(tokenizer),
+            n_ctx=64,
+            bos_token_id=tokenizer.bos_token_id,
+            eos_token_id=tokenizer.eos_token_id,
+        )
+    model = GPT2LMHeadModel(config)
+    model.from_pretrained(args.model_path)
+
+    data_collator = DataCollatorForLanguageModeling(tokenizer, mlm=False)
+    training_args = TrainingArguments(
+        output_dir="transformer-zerolm",
+        per_device_train_batch_size=args.batch_size,
+        per_device_eval_batch_size=args.batch_size//2,
+        evaluation_strategy="epoch",
+        gradient_accumulation_steps=8,
+        num_train_epochs=1,
+        weight_decay=0.1,
+        lr_scheduler_type="cosine",
+        learning_rate=5e-5,
+        fp16=True,
+        logging_strategy="epoch",
+        save_strategy="epoch",
+    )
+
+    trainer = Trainer(
+        model=model,
+        tokenizer=tokenizer,
+        args=training_args,
+        data_collator=data_collator,
+        train_dataset=tokenized_datasets["valid"],
+        eval_dataset=tokenized_datasets["valid"],
+    )
+
+    for lang in args.test_languages:
+        print(lang)
+        print(trainer.evaluate(
+            eval_dataset=tokenized_datasets[lang]
+        ))
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_dir", type=str, default="/content/drive/MyDrive/Colab Notebooks/zero-shot-lm/bibles_latin_csv/")
+    parser.add_argument("--model_path", type=str, required=True)
+
+    parser.add_argument("--test_split", type=str, default=None,
+                        help="red/blue/green/yellow group of languages")
+    parser.add_argument("--test_language", type=str, default=None,
+                        help="individual language")
+    parser.add_argument("--test_file", type=str, default=None,
+                        help="txt file with one language on each line")
+    parser.add_argument("--batch_size", type=int, default=32)
+
+    args = parser.parse_args()
+
+    # Need exactly 1 way to specify val language
+    assert((args.test_split is not None) + (args.test_language is not None) + (args.test_file is not None) == 1)
+    test_languages = []
+    if args.test_split is not None:
+        test_languages = langs_dict[args.test_split]
+    elif args.test_language is not None:
+        test_languages.append(args.test_language)
+    elif args.test_file is not None:
+        with open(args.test_file, 'r') as f:
+            for line in f:
+                test_languages.append(line.strip())
+    args.test_languages = test_languages
+    args.train_languages = []
+
+    main(args)
