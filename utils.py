3cdb4b1da94310f24066b6f11cec32ee2361ae25
Sedrick Keh <sedrickkeh@gmail.com>
2022-06-09 04:07:50 -0400

    change back to int for now ... no idea why error

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ def load_lang2vec(langvec_dir="/content/drive/MyDrive/Colab Notebooks/zero-shot-
         for line in f:
             langsplit = line.strip().split('\t')
             lang, vec = langsplit[0], langsplit[1:]
-            vec = [float(i) for i in vec]
+            vec = [int(float(i)) for i in vec]
             lang2vec[lang] = vec
     return lang2vec
     
